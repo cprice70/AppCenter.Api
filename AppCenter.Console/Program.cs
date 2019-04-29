2f3a315f5e3aeaca5d8cabf182d162178f12f0b6
cprice1970 <cprice1@lci1.com>
2019-04-29 18:23:50 -0400

    Initial check in

---
+++ AppCenter.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using AppCenter.Api;
+using Refit;
+
+namespace AppCenter.Console
+{
+    class Program
+    {
+        static readonly string api_token = "27d66c8af84990fa93d03ae51cae88cc8b32622e";//X-API-Token = "";
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var appCenterApiUser = RestService.For<IAccount>("https://api.appcenter.ms");
+                var appCenterApiDist = RestService.For<IDistribute>("https://api.appcenter.ms");
+
+                var user = await appCenterApiUser.GetUser();
+                System.Console.WriteLine(user.Name);
+
+                var connection = await appCenterApiUser.GetServiceConnections();
+
+                var users = await appCenterApiUser.GetUsers("onecontrol-w4m3");
+
+                var testers = await appCenterApiUser.GetTesters("onecontrol-w4m3");
+
+                var releases = await appCenterApiDist.GetAppReleases("onecontrol-w4m3", "OneControl-V3-Android");
+
+                var recent_releases = await appCenterApiDist.GetAppRecentReleases("onecontrol-w4m3", "OneControl-V3-Android");
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"{ex}");
+            }
+        }
+    }
+}
